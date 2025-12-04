378ae3cb2ddc456d8d92c4a354bf9d47ac81c014
vignesh <vvvhcdsvc@gmail.com>
2025-12-04 09:04:34 +0000

    Update node.cpp

--- library/node.cpp
+++ library/node.cpp
@@ -1,7 +1,11 @@
 #include"class.h"
 #include<sys/wait.h>
-int buffer=0;
-int deletes=0;
+int buffer_node=0;
+int deletes_node=0;
+ui interface;
+user*user_head_ptr=NULL;
+st*head_ptr=NULL;
+
 	 	void node_act::add(const char*pre)
         	{
 			
@@ -14,32 +18,32 @@ int deletes=0;
 				{
 					st*temp;
 					temp =new st;
-        	       			cout<<"Enter the book id:";
+        	       			cout<<"  Enter the book id:";
 					cin>>temp->book_id;
-					cout<<"Enter the book name:";
+					cout<<"  Enter the book name:";
 					cin>>temp->book_name;
 					temp->left=NULL;
 					temp->right=NULL;
 					temp->book_count=temp->book_count+1;
         	       			add_books(&head_ptr,temp);
 					cout<<temp->book_count<<endl;
-					//system("clear");
-					//print_tree(head_ptr);
+	
+					
 				}
 			}
 			else if(strcmp(pre,"Issuse Book\0")==0)
 			{
 				user*temp;
 				temp=(user*)malloc(sizeof(user));
-				cout<<"Enter the book_id :";
+				cout<<"  Enter the book_id :";
 				cin>>temp->book_id;
-				cout<<"Enter the user_id :";
+				cout<<"  Enter the user_id :";
 				cin>>temp->user_id;
-				cout<<"Enter issue_date :";
+				cout<<"  Enter issue_date :";
 				cin>>temp->issue_date;
 				temp->due_date=temp->issue_date+7;
 				add_issue(&user_head_ptr,temp);
-				print_issue(user_head_ptr);
+
 
 			}
 
@@ -51,10 +55,10 @@ int deletes=0;
 			wait(&status);
 
 
-			if(deletes !=0)
+			if(deletes_node !=0)
 			{
 				deleted_add(*node,new_node);
-				deletes--;
+				deletes_node--;
 				return;
 			}
 			if(*node==NULL)
@@ -94,17 +98,18 @@ int deletes=0;
                                 add_issue((&(*node)->left),new_node);
                         }
                 }
-		void node_act::print_books(st*head_node)
+		void node_act::print_books(st*head_ptr)
 		{
 			
-			if(head_node!=NULL)
+			if(head_ptr!=NULL)
 			{
+				cout<<" "<<endl;
 
 	    			//cout<<head_node->book_name<<"->";
 	    			//cout<<head_node->book_id<<"->";
-				interface.show_ui(head_node->book_id,head_node->book_name);
-				print_books(head_node->left);
-				print_books(head_node->right);
+				interface.show_ui(head_ptr->book_id,head_ptr->book_name);
+				print_books(head_ptr->left);
+				print_books(head_ptr->right);
 			
 
 			}
@@ -114,16 +119,16 @@ int deletes=0;
 				return;
 			}
 		}
-		void node_act::print_issue(user*head_node)
+		void node_act::print_issue(user*user_head_ptr)
                 {
 
-                        if(head_node!=NULL)
+                        if(user_head_ptr!=NULL)
                         {
 
                                 //cout<<head_node->book_id<<"->";
-				interface.show_ui(head_node->book_id,head_node->book_name);
-                                print_issue(head_node->left);
-                                print_issue(head_node->right);
+				interface.show_ui(user_head_ptr->book_id,user_head_ptr->book_name);
+                                print_issue(user_head_ptr->left);
+                                print_issue(user_head_ptr->right);
 
 
                         }
@@ -142,8 +147,8 @@ int deletes=0;
 			{
 				return head_ptr;
 			}
-		       	search_node(head_ptr->left,id,buffer);
-			search_node(head_ptr->right,id,buffer);
+		       	return search_node(head_ptr->left,id,buffer);
+			return search_node(head_ptr->right,id,buffer);
 		}
 		user* node_act::user_search_node(user*head_ptr,int id,string buffer)
                 {
@@ -153,8 +158,8 @@ int deletes=0;
                         {
                                 return head_ptr;
                         }
-                        user_search_node(head_ptr->left,id,buffer);
-                        user_search_node(head_ptr->right,id,buffer);
+                        return user_search_node(head_ptr->left,id,buffer);
+                        return user_search_node(head_ptr->right,id,buffer);
                 }
 
 		void node_act::update_node(st*head_ptr,int id,char* book)
@@ -194,7 +199,7 @@ int deletes=0;
                         {
 				node->book_id =9999;
 				node->book_name=" ";
-				deletes++;
+				deletes_node++;
                                 return ;
                         }
                         delete_node(node->left,id);
